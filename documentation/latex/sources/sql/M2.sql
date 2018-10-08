20d3090890d8b1ea95856a5cda12e66016c5b53d
daniel_san <danielecuomo@github.com>
2018-10-08 18:47:01 +0200

    some fixes

--- documentation/latex/sources/sql/M2.sql
+++ documentation/latex/sources/sql/M2.sql
@@ -1,4 +1,4 @@
-SELECT P.test_id, AVG(P.co2_mass_g_s), AVG(P.nox_mass_g_s), AVG(P.power_kw), AVG(P.speed_km_h)
+SELECT P.test_id, AVG(P.co2_mass_g_s), AVG(P.nox_mass_g_s), AVG(G.power_kw), AVG(G.speed_km_h)
 FROM acquisition_fact_particles P, acquisition_fact_general G
 WHERE P.row_id = G.row_id
-GROUP BY P.test_id
+GROUP BY P.test_id;
